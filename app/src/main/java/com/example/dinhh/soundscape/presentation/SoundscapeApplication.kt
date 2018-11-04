aa469a37e85d40972f6eaa56e24df2a218f9cfbc
HuyTrinh <dinhhuyams@gmail.com>
2018-11-04 14:12:20 +0200

    Add retrofit, rxjava, architecture components libs

---
+++ app/src/main/java/com/example/dinhh/soundscape/presentation/SoundscapeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dinhh.soundscape.presentation
+
+import android.app.Application
+import android.util.Log
+
+class SoundscapeApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
