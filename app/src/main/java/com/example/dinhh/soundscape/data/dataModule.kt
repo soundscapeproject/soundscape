76e82b66fa353ec86d0dc1ecfab8be7857486597
HuyTrinh <dinhhuyams@gmail.com>
2018-11-09 21:48:25 +0200

    Add local database to dataModule@

--- app/src/main/java/com/example/dinhh/soundscape/data/dataModule.kt
+++ app/src/main/java/com/example/dinhh/soundscape/data/dataModule.kt
@@ -1,5 +1,10 @@
 package com.example.dinhh.soundscape.data
 
+import android.arch.persistence.room.Room
+import com.example.dinhh.soundscape.data.local.Database
+import com.example.dinhh.soundscape.data.local.DatabaseConfig
+import com.example.dinhh.soundscape.data.local.SoundscapeLocalData
+import com.example.dinhh.soundscape.data.local.SoundscapeLocalDataImpl
 import com.example.dinhh.soundscape.data.pref.SharedPref
 import com.example.dinhh.soundscape.data.pref.SharedPrefImpl
 import com.example.dinhh.soundscape.data.remote.SoundscapeApi
@@ -9,6 +14,7 @@ import com.example.dinhh.soundscape.data.repository.UserRepository
 import com.example.dinhh.soundscape.data.repository.UserRepositoryImpl
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -17,11 +23,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 val dataModule = module {
     factory<UserRepository> {
         UserRepositoryImpl(get(), get())
-    }
-
-    //SharedPref
-    factory<SharedPref> {
-        SharedPrefImpl(get())
     }
 
     //Remote
@@ -48,4 +49,26 @@ val dataModule = module {
             .build()
             .create(SoundscapeApi::class.java)
     }
+
+    //Local
+    factory<SharedPref> {
+        SharedPrefImpl(get())
+    }
+
+    factory<SoundscapeLocalData> {
+        SoundscapeLocalDataImpl(get())
+    }
+
+    factory { get<Database>().recordDao() }
+
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            Database::class.java,
+            DatabaseConfig.DATABASE_NAME
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
 }
