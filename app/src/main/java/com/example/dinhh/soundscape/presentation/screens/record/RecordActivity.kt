da20e9cac4ba1c3aa0d8be9525816baf0d643786
HuyTrinh <dinhhuyams@gmail.com>
2018-11-10 12:47:05 +0200

    Migrate code from RecordFragment to RecordActivity

--- app/src/main/java/com/example/dinhh/soundscape/presentation/screens/record/RecordActivity.kt
+++ app/src/main/java/com/example/dinhh/soundscape/presentation/screens/record/RecordActivity.kt
@@ -1,28 +1,87 @@
 package com.example.dinhh.soundscape.presentation.screens.record
 
+import android.arch.lifecycle.Observer
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Toast
 import com.example.dinhh.soundscape.R
+import com.example.dinhh.soundscape.common.invisible
+import com.example.dinhh.soundscape.common.isVisible
 import com.example.dinhh.soundscape.common.logD
+import com.example.dinhh.soundscape.common.visible
 import com.example.dinhh.soundscape.presentation.dialog.SaveDialog
 import kotlinx.android.synthetic.main.activity_record.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class RecordActivity : AppCompatActivity(), SaveDialog.SaveDialogListener {
 
-    override fun onSaveDialogPositiveClick(recordName: String, category: String) {
-        logD("SAVE DIALOG: ${recordName} : ${category}")
-    }
-
-    override fun onSaveDialogNegativeClick(dialog: SaveDialog) {
-        logD("CANCEL DIALOG")
-    }
+    private val recordViewModel: RecordViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record)
 
+        recordViewModel.viewState.observe(this, Observer {
+            it?.run(this@RecordActivity::handleView)
+        })
+
+        setupView()
+
+        handleButtonClicked()
+
         btnSave.setOnClickListener {
             showSaveDialog()
+        }
+    }
+
+    private fun setupView() {
+        btnStopRecording.invisible()
+        btnPlay.invisible()
+        recordingTextView.invisible()
+        btnSave.invisible()
+    }
+
+    private fun handleView(viewState: RecordViewState) = when (viewState) {
+        is RecordViewState.Success -> {
+            //Success State
+        }
+
+        is RecordViewState.Failure -> {
+            Toast.makeText(this, "Error: ${viewState.throwable.localizedMessage}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun handleButtonClicked() {
+        btnStartRecording.setOnClickListener {
+            btnStartRecording.invisible()
+            btnStopRecording.visible()
+            recordingTextView.visible()
+            chronometer.base = SystemClock.elapsedRealtime()
+            chronometer.start()
+            recordViewModel.startRecording()
+        }
+
+        btnStopRecording.setOnClickListener {
+            btnStartRecording.visible()
+            btnStopRecording.invisible()
+            recordingTextView.invisible()
+            chronometer.stop()
+            if (!btnPlay.isVisible()) {
+                btnPlay.visible()
+                btnSave.visible()
+            }
+            recordViewModel.stopRecording()
+        }
+
+        btnPlay.setOnClickListener {
+            recordViewModel.playRecord()
+        }
+
+        btnSave.setOnClickListener {
+            val intent = Intent(this, RecordActivity::class.java)
+            startActivity(intent)
         }
     }
 
@@ -31,5 +90,13 @@ class RecordActivity : AppCompatActivity(), SaveDialog.SaveDialogListener {
         val saveDialog = SaveDialog.newInstance("Save Record")
 
         saveDialog.show(supportFragmentManager, "SAVE_DIALOG")
+    }
+
+    override fun onSaveDialogPositiveClick(recordName: String, category: String) {
+        logD("SAVE DIALOG: ${recordName} : ${category}")
+    }
+
+    override fun onSaveDialogNegativeClick(dialog: SaveDialog) {
+        logD("CANCEL DIALOG")
     }
 }
