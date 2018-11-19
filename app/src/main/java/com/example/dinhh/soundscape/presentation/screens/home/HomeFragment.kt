f405cff27efbc842c93dd941daeb027e95680ba1
HuyTrinh <dinhhuyams@gmail.com>
2018-11-19 22:34:20 +0200

    Setup my library list

--- app/src/main/java/com/example/dinhh/soundscape/presentation/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/dinhh/soundscape/presentation/screens/home/HomeFragment.kt
@@ -1,22 +1,52 @@
 package com.example.dinhh.soundscape.presentation.screens.home
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.example.dinhh.soundscape.R
+import com.example.dinhh.soundscape.common.logD
+import com.example.dinhh.soundscape.presentation.base.RecyclerViewListener
+
+val myLibraryTitle = listOf(
+    "Saved Soundscapes",
+    "Recorded Tracks",
+    "Favourite Sounds"
+)
 
 class HomeFragment : Fragment() {
+
+    private lateinit var adapter: LibararyAdapter
+    private lateinit var rvLibrary: RecyclerView
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        rvLibrary = view.findViewById(R.id.rvLibrary) as RecyclerView
+
+        setupViews()
+
+        return view
+    }
+
+    private fun setupViews() {
+        adapter = LibararyAdapter(myLibraryTitle)
+        adapter.setOnItemClickListener(object : RecyclerViewListener.OnItemClickListener {
+            override fun onItemClick(view: View, position: Int) {
+            }
+        })
+        val layoutManager = LinearLayoutManager(context!!)
+        rvLibrary.layoutManager = layoutManager
+        rvLibrary.adapter = adapter
     }
 
 
