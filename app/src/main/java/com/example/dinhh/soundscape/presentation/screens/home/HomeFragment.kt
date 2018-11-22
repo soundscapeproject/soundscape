5358a02d086c5e4b373881966ebefd201a7643b2
Eero Karvonen <eerokar@metropolia.fi>
2018-11-22 13:58:31 +0200

    implemented UI for the library for most parts

--- app/src/main/java/com/example/dinhh/soundscape/presentation/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/dinhh/soundscape/presentation/screens/home/HomeFragment.kt
@@ -6,8 +6,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.example.dinhh.soundscape.R
+import com.example.dinhh.soundscape.presentation.screens.sounds.SoundFragment
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -15,10 +16,36 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupButtons()
+    }
+
+    private fun setupButtons(){
+        natureBtn.setOnClickListener {
+            goToSoundsFragment("nature")
+        }
+        humanBtn.setOnClickListener {
+            goToSoundsFragment("human")
+        }
+        machineBtn.setOnClickListener {
+            goToSoundsFragment("machine")
+        }
+        storyBtn.setOnClickListener {
+            goToSoundsFragment("story")
+        }
+
+    }
+
+    fun goToSoundsFragment(category: String){
+        val fragManager = fragmentManager
+        val fragmentTransaction = fragManager?.beginTransaction()
+        fragmentTransaction?.replace(R.id.container, SoundFragment.newInstance(category))
+        fragmentTransaction?.addToBackStack(null)?.commit()
+    }
 
     companion object {
         @JvmStatic
