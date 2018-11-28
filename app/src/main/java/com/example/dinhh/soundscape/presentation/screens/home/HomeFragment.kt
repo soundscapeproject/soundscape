eae630cca1bb14c4989228564fcdc968c1a695c3
eerokar <karvonen.eero@gmail.com>
2018-11-28 10:18:20 +0200

    Changed the contents of the home fragment to the library fragment

--- app/src/main/java/com/example/dinhh/soundscape/presentation/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/dinhh/soundscape/presentation/screens/home/HomeFragment.kt
@@ -7,8 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.dinhh.soundscape.R
-import com.example.dinhh.soundscape.presentation.screens.sounds.SoundFragment
-import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -21,31 +19,9 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupButtons()
-    }
-
-    private fun setupButtons(){
-        natureBtn.setOnClickListener {
-            goToSoundsFragment("nature")
-        }
-        humanBtn.setOnClickListener {
-            goToSoundsFragment("human")
-        }
-        machineBtn.setOnClickListener {
-            goToSoundsFragment("machine")
-        }
-        storyBtn.setOnClickListener {
-            goToSoundsFragment("story")
-        }
 
     }
 
-    private fun goToSoundsFragment(category: String){
-        val fragManager = fragmentManager
-        val fragmentTransaction = fragManager?.beginTransaction()
-        fragmentTransaction?.replace(R.id.container, SoundFragment.newInstance(category))
-        fragmentTransaction?.addToBackStack(null)?.commit()
-    }
 
     companion object {
         @JvmStatic
